50bb95fcca815781cb2606283b290749951c0db0
Ososese-A <contact.sese.a@gmail.com>
2024-03-15 14:23:48 +0200

    The navbar issue had been fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.svg';
+import '../styles/nav.css';
 
 const Navbar = () => {
     return ( 
